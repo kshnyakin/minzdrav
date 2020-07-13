34c14ad8b8b7407d67c7aaf9563e3ab2650b5306
Alexander Kshnyakin <a.kshnyakin@vniias.ru>
2020-07-14 00:24:39 +0300

    bug fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,58 +6,49 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+positions = Position.create([
+  { name: 'Директор'}, 
+  { name: 'Менеджер'},
+  { name: 'Системный администратор'}
+  ])
 
+admin = User.create({
+  surname: "Иванов",
+  name: "Иван",
+  patrynomic: "Иванович",
+  phone: "8-916-641-59-26",
+  position_id: 1,
+  empl_date: Time.now,
+  login: "admin",
+  email: "admin@minzdrav.ru",
+  password: "admin"
+})
 
+admin.add_role "admin"
 
+manager = User.create({
+  surname: "Менеджеров",
+  name: "Менеджер",
+  patrynomic: "Менеджерович",
+  phone: "911",
+  position_id: 2,
+  empl_date: Time.now,
+  login: "manager",
+  email: "manager@minzdrav.ru",
+  password: "manager"
+})
+manager.add_role "employee"
 
+worker = User.create({
+  surname: "Работящий",
+  name: "Макар",
+  patrynomic: "Петрович",
+  phone: "123-456",
+  position_id: 2,
+  empl_date: Time.now,
+  login: "worker",
+  email: "worker@minzdrav.ru",
+  password: "worker"
+})
 
-
-
-
-
-# positions = Position.create([
-#   { name: 'Директор'}, 
-#   { name: 'Менеджер'},
-#   { name: 'Системный администратор'}
-#   ])
-
-# admin = User.create({
-#   surname: "Иванов",
-#   name: "Иван",
-#   patrynomic: "Иванович",
-#   phone: "8-916-641-59-26",
-#   position_id: 1,
-#   empl_date: Time.now,
-#   login: "admin",
-#   email: "admin@minzdrav.ru",
-#   password: "admin"
-# })
-
-# admin.add_role "admin"
-
-# manager = User.create({
-#   surname: "Менеджеров",
-#   name: "Менеджер",
-#   patrynomic: "Менеджерович",
-#   phone: "911",
-#   position_id: 2,
-#   empl_date: Time.now,
-#   login: "manager",
-#   email: "manager@minzdrav.ru",
-#   password: "manager"
-# })
-# manager.add_role "employee"
-
-# worker = User.create({
-#   surname: "Работящий",
-#   name: "Макар",
-#   patrynomic: "Петрович",
-#   phone: "123-456",
-#   position_id: 2,
-#   empl_date: Time.now,
-#   login: "worker",
-#   email: "worker@minzdrav.ru",
-#   password: "worker"
-# })
-
-# worker.add_role "employee"
+worker.add_role "employee"
